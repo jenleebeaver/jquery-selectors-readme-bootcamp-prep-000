64a2010081154fd337f21fceb2a19170b1709b46
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-09 14:54:53 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -17,7 +17,7 @@ function divSelector() {
 }
 
 function firstListItem() {
-  return $('ul #pic-list li:first-child')
+  return $('ul#pic-list li:first-child')
 }
 // $(document).ready(paragraphSelector() {
 //   $('p-tags')
